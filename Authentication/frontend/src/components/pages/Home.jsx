d2209b20c6a006480c48892e9b45d0060686f202
Raj Bundela <rajrvsbbundela@gmail.com>
2022-01-22 09:17:52 +0530

    Merged Authentication Frontend and Backend

---
+++ Authentication/frontend/src/components/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useContext } from 'react';
+import { useHistory, Link } from 'react-router-dom';
+import UserContext from '../../context/userContext';
+
+function Home () {
+    const {userData} = useContext(UserContext);
+    const history = useHistory();
+
+    useEffect(() => {
+        if(!userData.user)
+            history.push("/login");
+
+    }, []);
+    return (
+        <div>
+            {userData.user ? (
+                <h3>Welcome {userData.user.displayName}</h3>
+            ) : (
+                <>
+                    <h5>You logged out</h5>
+                    <Link to="/login">Login</Link>
+                </>
+            )}
+        </div>
+    );
+}
+ 
+export default Home;
